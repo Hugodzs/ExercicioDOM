4d9c41c5b823962342873bd4f3444341314b7532
Hugodz <hugosilva1ads@gmail.com>
2024-01-11 19:44:23 -0300

    atual

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ link.innerText = "Site da Proz"
 
 let lista = document.querySelector("ul")
 
-lista.innerHTML = 
-"<li>Item 1</li> <li>Item 2</li> <li>Item 3</li> <li>Item 4</li>"
+lista.innerHTML = `
+<li>Item 1</li> 
+<li>Item 2</li> 
+<li>Item 3</li> 
+<li>Item 4</li>`
 
 
 let list2 = document.querySelector("#lista-ordenada")
 
-list2.innerHTML = 
-"<li>Item 1</li> <li>Item 2</li> <li>Item 3</li> <li>Item 4</li>"
+list2.innerHTML = `
+<li><a href="https://dio.com/">Meta</a></li>
+<li><a href="https://www.facebook.com>AWS</a></li>
+<li><a href="https://www.google.com">Google</a></li>
+`
