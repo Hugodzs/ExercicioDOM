7c5defdd4ac870dc079798eba7e615c5552d1028
Hugodz <hugosilva1ads@gmail.com>
2024-01-11 19:27:56 -0300

    Exercicio

---
+++ script.js
@@ -0,0 +1,18 @@
+let titulo = document.querySelector("#titulo")
+
+titulo.innerText = "Hugo agora vai"
+
+let link = document.querySelector("a")
+
+link.innerText = "Site da Proz"
+
+let lista = document.querySelector("ul")
+
+lista.innerHTML = 
+"<li>Item 1</li> <li>Item 2</li> <li>Item 3</li> <li>Item 4</li>"
+
+
+let list2 = document.querySelector("#lista-ordenada")
+
+list2.innerHTML = 
+"<li>Item 1</li> <li>Item 2</li> <li>Item 3</li> <li>Item 4</li>"
